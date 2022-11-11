6956301d6c65676cb5e55081f8fc019be6b79829
Mary Danyliuk <marydanyliuk@gmail.com>
2022-11-11 13:59:43 +0200

    02-video.js in progress

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,30 @@
-const onPlay = function (data) {
-  // data is an object containing properties specific to that event
-};
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
-player.on('play', onPlay);
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onPlay = function (data) {
+  console.log('the time was updated to: ', +data.seconds);
+};
+player.on('timeupdate', onPlay);
+
+('videoplayer-current-time');
+
+player
+  .getCurrentTime()
+  .then(function (seconds) {
+    console.log(+seconds, 'current seconds ');
+    // seconds = the current playback position
+  })
+  .catch(function (error) {
+    // an error occurred
+  });
+
+// player.on('play', onPlay);
